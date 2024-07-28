673bc5c9be20a1cf942e68db3eba35115f7cc2c1
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2024-07-28 20:03:52 +0000

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,56 +1,53 @@
-
-dule to fetch user information and export TODO list to a CSV file
+#!/usr/bin/python3
+"""
+    Python script that exports data in the CSV format
 """
 import csv
+import json
 import requests
 from sys import argv
 
 
-def get_employee_info(employee_id):
+if __name__ == "__main__":
     """
-    Get employee information by employee ID
+        Request user info by employee ID
     """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    response = requests.get(url)
-    return response.json()
-
-
-def get_employee_todos(employee_id):
+    request_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     """
-    Get the TODO list of the employee by employee ID
+        Convert json to dictionary
     """
-    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-    response = requests.get(url)
-    return response.json()
-
-
-def export_to_csv(employee_id, username, todos):
+    user = json.loads(request_employee.text)
     """
-    Export TODO list to a CSV file
+        Extract username
     """
-    filename = f'{employee_id}.csv'
-    with open(filename, mode='w') as file:
-        file_writer = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            rowData = [employee_id, username, todo['completed'], todo['title']]
-            file_writer.writerow(rowData)
-
-
-def main(employee_id):
-    """
-    Main function to fetch user info and TODO list, then export to CSV
-    """
-    user = get_employee_info(employee_id)
     username = user.get("username")
 
-    todos = get_employee_todos(employee_id)
+    """
+        Request user's TODO list
+    """
+    request_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+    """
+        Dictionary to store task status(completed) in boolean format
+    """
+    tasks = {}
+    """
+        Convert json to list of dictionaries
+    """
+    user_todos = json.loads(request_todos.text)
+    """
+        Loop through dictionary & get completed tasks
+    """
+    for dictionary in user_todos:
+        tasks.update({dictionary.get("title"): dictionary.get("completed")})
 
-    export_to_csv(employee_id, username, todos)
+    """
+        Export to CSV
+    """
+    with open('{}.csv'.format(argv[1]), mode='w', newline='') as file:
+        file_editor = csv.writer(file, delimiter=',', quoting=csv.QUOTE_ALL)
+        for k, v in tasks.items():
+            file_editor.writerow([argv[1], username, v, k])
 
-
-if __name__ == "__main__":
-    if len(argv) > 1:
-        main(argv[1])
-    else:
-        print("Usage: ./1-export_to_CSV.py <employee_id>")
-
+    print('Data exported to {}.csv'.format(argv[1]))
